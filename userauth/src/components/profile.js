6c7dccb9c09d8f7ff944222e635ccb02f1d2fc6c
Joel Denny Joseph <iamjoeldenny@gmail.com>
2021-07-14 19:03:02 +0530

    SignOut added

--- userauth/src/components/profile.js
+++ userauth/src/components/profile.js
@@ -1,16 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import firebase from 'firebase';
 
 function Profile() {
+    const[email,setEmail] = useState('');
+    const history = useHistory();
+    
+    const SignOut = async () => {
+        await firebase.auth().signOut().then(() => {
+            console.log("Sign Out success");
+            history.goBack();
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          setEmail(user.email);
+        } else {
+            setEmail('');
+        }
+      });
+
     return (
         <div className='container'>
             <h1>Your Profile</h1>
             <h3>Name</h3>
             <h3>Age</h3>
             <h3>Address</h3>
-            <h3>Email</h3>
+            <h3>Email:{email}</h3>
 
-            <Link to='/Login'>LOG IN</Link>
+            <button onClick={ ()=>{ SignOut() } }>
+                SIGN OUT
+            </button>
         </div>
     )
 }
