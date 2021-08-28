ee47b6a7d919dc7957198582d92990af4a20c68e
Joel Denny Joseph <iamjoeldenny@gmail.com>
2021-08-28 23:30:51 +0530

    Data Fetching initialised

--- dictionary/src/App.js
+++ dictionary/src/App.js
@@ -1,6 +1,29 @@
+import axios from 'axios';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [word, setWord] =  useState("");
+  const[meanings, setMeanings] = useState([]);
+
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/plane`
+      );
+
+      setMeanings(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  console.log(meanings);
+  
+  useEffect(() => {
+    dictionaryApi();
+  }, []);
+
   return (
     <div className="App">
       Basic App Initialisation
